6ccb6a7d6a2ac7479969c49b4202efdff4cc7703
Jeff Stubler <brunsa2@gmail.com>
2014-06-22 14:33:30 -0500

    Add cached matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+#' Matrix with cached inverse. Provides function to create matrix that can cache inverse
+#' and function to cache inverse or use cached version
+#' @author Jeff Stubler
 
-## Write a short comment describing this function
-
+#' Makes a matrix representation with accessor and mutator pairs for the matrix and
+#' for its inverse.
+#' 
+#' @param x Matrix to create cached version of
+#' 
+#' @return List of get and set functions for both matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(new_matrix) {
+        x <<- new_matrix
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(new_inverse) inverse <<- new_inverse
+    getinverse <- function() inverse
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+#' Solves for the matrix inverse and caches it, or returns the cached version
+#' if available.
+#' 
+#' @param x Cached matrix to calculate inverse of
+#' 
+#' @return Calculated or cached inverse of matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    current_inverse <- x$getinverse()
+    if (!is.null(current_inverse)) {
+        message("Using cached version")
+        return(current_inverse)
+    } else {
+        current_matrix <- x$get()
+        calculated_inverse <- solve(current_matrix, ...)
+        x$setinverse(calculated_inverse)
+        return(calculated_inverse)
+    }
 }
